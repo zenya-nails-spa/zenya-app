4b6252bb2fc844f44ec2cf561572dc404817bcdb
Carlos Roberto Martinez Ojeda <carlos.1424@live.com.mx>
2025-09-21 06:25:01 -0600

    dummy commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ const App = () => {
   return (
     <main style={{ padding: '2rem', maxWidth: 860, margin: '0 auto' }}>
       <h1>Zenyaasdfa Nails & Spa</h1>
-      <p>Bienvenidas a Zenya Nails & Spa 🌸</p>
+      <p>Bienvenidasssss a Zenya Nails & Spa 🌸</p>
       <p>
         <a href="privacy-policy.html" target="_blank" rel="noopener">
           Ver Política de Privacidad
